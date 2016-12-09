6f61f26cc715ded3561588e1cab25e0ec316ce4d
Karl Ehatäht <karlehataht@gmail.com>
2016-12-09 14:58:36 +0200

    Refactor client code

--- client.py
+++ client.py
@@ -4,81 +4,106 @@ logging.basicConfig()
 
 # Global constants -----------------------------------------------
 GAME_SERVER_NAME = 'Server'
+
+# Synchronization primitives -------------------------------------
 global_bool = False
 initialization_phase = True
 is_alive = True
 counter = 0
 queue = Queue.Queue()
 cv = threading.Condition()
-temp_dict = {}
-game_board = []
-BOARD_WIDTH = 10
-BOARD_HEIGHT = 10
 
+# Game-specific variables ----------------------------------------
+temp_dict  = {}
+game_board = []
+BOARD_WIDTH  = 10
+BOARD_HEIGHT = 10
 
 #def testing():
 #    server_list_con.add_timeout(5, testing)
 #    print('Timeout thingyu')
 
-server_list_con = pika.BlockingConnection(pika.ConnectionParameters(
-        host='localhost'))
+# Indirect communication channels --------------------------------
+server_list_con = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost')
+)
 #server_list_con.add_timeout(5, testing)
-server_list_ch = server_list_con.channel()
-
-
-server_bcasts_con = pika.BlockingConnection(pika.ConnectionParameters(
-        host='localhost'))
+server_bcasts_con = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost')
+)
+server_list_ch   = server_list_con.channel()
 server_bcasts_ch = server_bcasts_con.channel()
 
+class TimedSet(set):
+    '''Set class with timed autoremoving of elements
+    Code taken from: http://stackoverflow.com/a/16137224
 
-
-class TimedSet(set): # http://stackoverflow.com/questions/16136979/set-class-with-timed-auto-remove-of-elements
-    def __init__(self): # For maintaining available server list
+    Needed for maintaining available server list
+    '''
+    def __init__(self):
         self.__table = {}
+
     def add(self, item, timeout=5):
         self.__table[item] = time.time() + timeout
         set.add(self, item)
+
     def __contains__(self, item):
         return time.time() < self.__table.get(item)
+
     def __iter__(self):
         for item in set.__iter__(self):
             if time.time() < self.__table.get(item):
                 yield item
 
-
 def listen_public_announcements():
-    server_list_ch.exchange_declare(exchange='announcements',
-                             type='fanout')
-    result = server_list_ch.queue_declare(exclusive=True)
+    server_list_ch.exchange_declare(
+        exchange = 'announcements',
+        type     = 'fanout'
+    )
+    result = server_list_ch.queue_declare(exclusive = True)
     queue_name = result.method.queue
 
-    server_list_ch.queue_bind(exchange='announcements',
-                       queue=queue_name)
-    server_list_ch.basic_consume(public_announc_callback,
-                          queue=queue_name,
-                          no_ack=True)
+    server_list_ch.queue_bind(
+        exchange = 'announcements',
+        queue    = queue_name
+    )
+    server_list_ch.basic_consume(
+        public_announc_callback,
+        queue  = queue_name,
+        no_ack = True
+    )
 
     print('Getting list of available servers, please wait...')
     print('If no servers are seen after 10 seconds, then no server is available.')
-    server_list_ch.start_consuming()
-
+    try:
+        server_list_ch.start_consuming()
+    except KeyboardInterrupt:
+        server_list_ch.stop_consuming()
 
 def listen_server_bcasts():
-    server_bcasts_ch.exchange_declare(exchange=GAME_SERVER_NAME,
-                              type='fanout')
+    server_bcasts_ch.exchange_declare(
+        exchange = GAME_SERVER_NAME,
+        type     = 'fanout'
+    )
 
-    result2 = server_bcasts_ch.queue_declare(exclusive=True)
+    result2 = server_bcasts_ch.queue_declare(exclusive = True)
     queue_name2 = result2.method.queue
 
-    server_bcasts_ch.queue_bind(exchange=GAME_SERVER_NAME,
-                        queue=queue_name2)
-    server_bcasts_ch.basic_consume(server_bcasts_callback,
-                           queue=queue_name2,
-                           no_ack=True)
+    server_bcasts_ch.queue_bind(
+        exchange = GAME_SERVER_NAME,
+        queue    = queue_name2
+    )
+    server_bcasts_ch.basic_consume(
+        server_bcasts_callback,
+        queue  = queue_name2,
+        no_ack = True
+    )
     common.clear_screen()
-    print(' [*] Broadcasts started listening. To exit press CTRL+C')
-    server_bcasts_ch.start_consuming()
-
+    print('Broadcasts started listening. To exit press CTRL+C')
+    try:
+        server_bcasts_ch.start_consuming()
+    except KeyboardInterrupt:
+        server_bcasts_ch.stop_consuming()
 
 def public_announc_callback(ch, method, properties, body):
     t_set.add(body)
@@ -95,8 +120,7 @@ def public_announc_callback(ch, method, properties, body):
                 print(t)
                 global temp_dict
                 temp_dict[t] = []
-            x = raw_input('\nWould you like to update the list? (y/n) \n')
-            if x == 'y':
+            if common.query_yes_no("Would you like to update the list?"):
                 print('Updating...')
                 initialization_phase = True
             else:
@@ -110,9 +134,9 @@ def server_bcasts_callback(ch, method, properties, body):
     elif CTRL_CODE == common.CTRL_START_GAME:
         board = common.unmarshal(rpc_client.call(common.marshal(common.CTRL_REQ_BOARD)))
         global game_board, BOARD_WIDTH, BOARD_HEIGHT
-        BOARD_WIDTH = int(board[2])
+        BOARD_WIDTH  = int(board[2])
         BOARD_HEIGHT = int(board[1])
-        game_board = np.fromstring(board[0], dtype=int).reshape(BOARD_HEIGHT,BOARD_WIDTH)
+        game_board = np.fromstring(board[0], dtype = int).reshape(BOARD_HEIGHT, BOARD_WIDTH)
         common.print_board(game_board, BOARD_WIDTH, BOARD_HEIGHT)
     else:
         cv.acquire()
@@ -122,14 +146,18 @@ def server_bcasts_callback(ch, method, properties, body):
 
 class RpcClient(object):
     def __init__(self):
-        self.rpc_con = pika.BlockingConnection(pika.ConnectionParameters(
-            host='localhost'))
-        self.rpc_ch = self.rpc_con.channel()
-        self.rpc_result = self.rpc_ch.queue_declare(exclusive=True)
+        self.rpc_con = pika.BlockingConnection(
+            pika.ConnectionParameters(host = 'localhost')
+        )
+        self.rpc_ch         = self.rpc_con.channel()
+        self.rpc_result     = self.rpc_ch.queue_declare(exclusive=True)
         self.callback_queue = self.rpc_result.method.queue
 
-        self.rpc_ch.basic_consume(self.on_response, no_ack=True,
-                                   queue=self.callback_queue)
+        self.rpc_ch.basic_consume(
+            self.on_response,
+            no_ack = True,
+            queue  = self.callback_queue
+        )
 
     def on_response(self, ch, method, props, body):
         if self.corr_id == props.correlation_id:
@@ -138,17 +166,18 @@ class RpcClient(object):
     def call(self, n):
         self.response = None
         self.corr_id = str(uuid.uuid4())
-        self.rpc_ch.basic_publish(exchange='',
-                                   routing_key='rpc_queue',
-                                   properties=pika.BasicProperties(
-                                         reply_to = self.callback_queue,
-                                         correlation_id = self.corr_id,
-                                         ),
-                                   body=str(n))
+        self.rpc_ch.basic_publish(
+            exchange    = '',
+            routing_key = 'rpc_queue',
+            properties  = pika.BasicProperties(
+                reply_to       = self.callback_queue,
+                correlation_id = self.corr_id,
+            ),
+            body        = str(n)
+        )
         while self.response is None:
             self.rpc_con.process_data_events()
         return self.response
-
 
 def do_rpc():
     while is_alive:
@@ -166,7 +195,6 @@ def do_rpc():
             pass
         cv.release()
 
-
 def authenticate():
     global temp_dict, GAME_SERVER_NAME
     boolean = True
@@ -181,7 +209,7 @@ def authenticate():
     GAME_SERVER_NAME = server_name
     common.clear_screen()
 
-    print('Connected to ' + str(GAME_SERVER_NAME))
+    print('Connected to {game_server_name}'.format(game_server_name = GAME_SERVER_NAME))
     while not boolean:
         u_name = raw_input("Enter your username:\n")
         pwd = raw_input("Enter your password:\n")
@@ -200,37 +228,43 @@ def authenticate():
 if __name__ == '__main__':
     rpc_client = RpcClient()
     t_set = TimedSet()
-    threads = []
-    game_board = []
-    get_servers_list_th = threading.Thread(target=listen_public_announcements, name='Listen_Public_Announc')
-    listen_server_bcasts_th = threading.Thread(target=listen_server_bcasts, name='Listen_Server_Bcasts')
-    rpc_thread = threading.Thread(target=do_rpc, name='RPC_Handling')
-    threads.extend((rpc_thread, listen_server_bcasts_th, get_servers_list_th))
 
-    get_servers_list_th.setDaemon(True)
+    game_board = []
+    get_servers_list_th = threading.Thread(
+        target = listen_public_announcements,
+        name   = 'Listen_Public_Announc'
+    )
+    listen_server_bcasts_th = threading.Thread(
+        target = listen_server_bcasts,
+        name   = 'Listen_Server_Bcasts'
+    )
+    rpc_thread = threading.Thread(
+        target = do_rpc,
+        name   = 'RPC_Handling'
+    )
+    threads = [rpc_thread, listen_server_bcasts_th, get_servers_list_th]
+    for t in threads:
+        t.setDaemon(True)
+
     get_servers_list_th.start()
     get_servers_list_th.join() # After server has been chosen, this thread stops
 
     u_name, player_id = authenticate()
-    print('Hello '+str(u_name)+' you have connected succesfully!')
+    print('Hello, {username}! You have connected succesfully!'.format(username = u_name))
 
     # If is admin: - start new game
     #board_w_shape = rpc_client.call(':'.join((str(player_id), str(CTRL_REQ_BOARD))))
     #board, shapex, shapey = board_w_shape.split(':')
     #print(np.fromstring(board, dtype=int).reshape(int(shapex), int(shapey)))
 
-    listen_server_bcasts_th.setDaemon(True)
-    rpc_thread.setDaemon(True)
     listen_server_bcasts_th.start()
 
     if player_id == 1:  # Admin has player id 1
-        boolean = True
-        while boolean:
-            x = raw_input("Do you want to start the game? (y/n)")
-            if x == 'y':
+        game_not_started = True
+        while game_not_started:
+            if common.query_yes_no("Do you want to start the game?"):
                 rpc_client.call(common.marshal(common.CTRL_START_GAME, player_id))
-                boolean = False
-
+                game_not_started = False
 
     #rpc_thread.start()
     while not global_bool:
--- common.py
+++ common.py
@@ -1,4 +1,4 @@
-import logging, functools, os, string
+import logging, functools, os, string, sys
 
 
 # Glboal constants ----------------------------------
@@ -55,3 +55,40 @@ def synchronized_g(lock):
                 lock.release()
         return decorator
     return wrapper
+
+import sys
+
+def query_yes_no(question, default = "yes"):
+    '''Ask a yes/no question via raw_input() and return their answer.
+    :param question: string, presented to the user
+    :param default:  string, presumed answer
+    :return: True, if the answer is ,,yes'' or similar,
+             False otherwise
+
+    Taken from: http://stackoverflow.com/a/3041990
+    '''
+    valid = {
+        "yes": True,
+        "y"  : True,
+        "ye" : True,
+        "no" : False,
+        "n"  : False
+    }
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = raw_input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' (or 'y' or 'n').\n")
